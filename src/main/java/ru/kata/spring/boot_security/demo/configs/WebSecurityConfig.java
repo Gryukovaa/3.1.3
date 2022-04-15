cad8ed5ad06c4d91d3e7ef32f37972dbcdf5ac1e
Gryukovaa <89275331+Gryukovaa@users.noreply.github.com>
2022-04-15 23:20:35 +0300

    с JWT

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -3,45 +3,50 @@ package ru.kata.spring.boot_security.demo.configs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.kata.spring.boot_security.demo.configs.JWT.JWTConfigurer;
+import ru.kata.spring.boot_security.demo.configs.JWT.JWTTokenProvider;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
+    @Autowired
+    private JWTTokenProvider jwtTokenProvider;
+    @Autowired
     private SuccessUserHandler successUserHandler;
+    @Autowired
     private UserService userService;
 
-    @Autowired
-    public WebSecurityConfig(SuccessUserHandler successUserHandler, UserService userService) {
-        this.successUserHandler = successUserHandler;
-        this.userService = userService;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .httpBasic().disable()
                 .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
-                    .antMatchers("/login").anonymous()
-                    .antMatchers("/user/**").access("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
-                    .antMatchers("/admin/**").access("hasAnyRole('ROLE_ADMIN')")
-                    .antMatchers("/").permitAll();
-        http
-                    .formLogin()
-                    .loginPage("/login")
-                    .successHandler(successUserHandler)
-                    .permitAll();
-               http
-                    .logout()
-                    .permitAll()
-                    .logoutSuccessUrl("/login");
+                .antMatchers("/v1/auth/login").permitAll()
+                .antMatchers("/user/**").hasRole("USER")
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                        .and()
+                                .apply(new JWTConfigurer(jwtTokenProvider));
     }
 
     @Override
@@ -54,4 +59,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder(12);
     }
 
+    /*@Bean
+    public JWTTokenProvider jwtTokenProvider(UserDetailsService userDetailsService) {
+        return new JWTTokenProvider(userDetailsService);
+    }*/
 }
